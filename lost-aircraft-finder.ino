465d17309b3e69700aee8a5020f9914f18a97aee
Miralem Halilović <miralem@gmail.com>
2018-03-04 19:14:06 +0100

    removed unused variables and added configurable stuff to the top

--- lost-aircraft-finder.ino
+++ lost-aircraft-finder.ino
@@ -1,15 +1,19 @@
-const int buzzerPin = 0;
-const int vibrationPin = 1;
-const int buzzingFrequency = 100; // the frquency of sound
-const int buzzingTime = 100;
-
-unsigned long previousMillis = 0;  
-unsigned long previousBuzzingMillis = 0;  
-
 // the time (ms) how log it will wait for vibrations, 
 // if no vibration during this time, it would mean that
 // the aircraft has landed or crashed
 const long interval = 15000;
+
+// buzzing and pause between buzing in ms
+const int buzzingTime = 100;
+
+
+// DONT CHANGE ANYtHING BELOW
+
+const int buzzerPin = 0;
+const int vibrationPin = 1;
+
+unsigned long previousMillis = 0;  
+unsigned long previousBuzzingMillis = 0;  
  
 bool testDone = false;
 
